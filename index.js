0226d18a996b84078ae124a814c1a887f3c86dde
Raul Sanz <luar_znas@hotmail.com>
2021-04-27 18:08:11 +0100

    JS: added the prompts and the html structure. HTML: created the template

---
+++ index.js
@@ -0,0 +1,46 @@
+const inquirer = require('inquirer');
+
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+const fs = require('fs');
+
+function startInquirer () {
+    const promptArray = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'What is your ID?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+      },
+      {
+        type: 'list',
+        name: 'title',
+        message: 'What is your title?',
+        choices: ["Manager", "Engineer", "Intern"]
+      }
+    ];
+    return inquirer
+    .prompt(promptArray);
+  };
+
+  
+  const init = () => {
+    startInquirer()
+      .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
+      .then(() => console.log('Successfully wrote to index.html'))
+      .catch((err) => console.error(err));
+  };
+  
+  init();
